019f65013c3e7252c798e1069e1a0bc1ae12b4d4
Alexia Pereira <alexia.pereira@cwi.com.br>
2018-10-16 23:12:54 -0300

    teste

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ express()
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.send('OK'))
     .get('/facebook/receive', (req, res) => {
-        debugger
-        if (req.query['hub.mode'] == 'subscribe') {
-            if (req.query['hub.verify_token'] == controller.config.verify_token) {
-                res.send(req.query['hub.challenge']);
-            } else {
-                res.send('OK');
-            }
-        }
+        // debugger
+        // if (req.query['hub.mode'] == 'subscribe') {
+        //     if (req.query['hub.verify_token'] == controller.config.verify_token) {
+        res.send(req.query['hub.challenge']);
+        // } else {
+        //     res.send('OK');
+        // }
+    }
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
