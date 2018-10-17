27d4fea78610fefb527ff39f3c2f88aaae38b9b0
Alexia Pereira <alexia.pereira@cwi.com.br>
2018-10-16 23:07:30 -0300

    teste

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('OK'))
-  .get('/facebook/receive', (req, res) => res.send('OK'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.send('OK'))
+    .get('/facebook/receive', (req, res) => {
+        if (req.query['hub.mode'] == 'subscribe') {
+            if (req.query['hub.verify_token'] == controller.config.verify_token) {
+                res.send(req.query['hub.challenge']);
+            } else {
+                res.send('OK');
+            }
+        }
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
