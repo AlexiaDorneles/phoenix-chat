0e5311237957297b55febadbd77b448ccd510af0
Alexia Pereira <alexia.pereira@cwi.com.br>
2018-10-16 23:10:12 -0300

    teste

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ express()
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.send('OK'))
     .get('/facebook/receive', (req, res) => {
+        debugger
         if (req.query['hub.mode'] == 'subscribe') {
             if (req.query['hub.verify_token'] == controller.config.verify_token) {
                 res.send(req.query['hub.challenge']);
